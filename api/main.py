ca68749d01a0550d8b400d52b8fe83d6c8e59154
Pamela Fox <pamela.fox@gmail.com>
2022-11-18 21:07:36 +0000

    Docs still work

--- api/main.py
+++ api/main.py
@@ -18,12 +18,3 @@ async def generate_name(starts_with: str = None, subscription_key: Union[str, No
         names = [n for n in names if n.lower().startswith(starts_with)]
     random_name = random.choice(names)
     return {"name": random_name}
-
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html(req: fastapi.applications.Request):
-    root_path = req.scope.get("root_path", "").rstrip("/")
-    openapi_url = root_path + "public/" + app.openapi_url
-    return fastapi.applications.get_swagger_ui_html(
-        openapi_url=openapi_url,
-        title="API",
-    )
