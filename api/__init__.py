014e6f46dc80afab30d3e3c3e680dbb1271cc43f
Pamela Fox <pamela.fox@gmail.com>
2022-11-18 21:08:24 +0000

    Formatting and logging

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,3 @@
-import logging
-
 import nest_asyncio
 import azure.functions as func
 
@@ -7,12 +5,7 @@ from .main import app
 
 nest_asyncio.apply()
 
-logging.getLogger().setLevel(logging.DEBUG)
-logging.getLogger('azure.functions.AsgiMiddleware').setLevel(logging.DEBUG)
-
 
 async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    logging.info(req.url)
     asgi_mware = func.AsgiMiddleware(app)
-    logging.getLogger('azure.functions.AsgiMiddleware').setLevel(logging.DEBUG)
     return asgi_mware.handle(req, context)
--- api/main.py
+++ api/main.py
@@ -4,15 +4,19 @@ from typing import Union
 import fastapi
 
 app = fastapi.FastAPI(
-    servers=[
-        {"url": "/api", "description": "API"}
-    ],
+    servers=[{"url": "/api", "description": "API"}],
     root_path="/public",
     root_path_in_servers=False,
 )
 
+
 @app.get("/generate_name")
-async def generate_name(starts_with: str = None, subscription_key: Union[str, None] = fastapi.Query(default=None, alias="subscription-key")):
+async def generate_name(
+    starts_with: str = None,
+    subscription_key: Union[str, None] = fastapi.Query(
+        default=None, alias="subscription-key"
+    ),
+):
     names = ["Minnie", "Margaret", "Myrtle", "Noa", "Nadia"]
     if starts_with:
         names = [n for n in names if n.lower().startswith(starts_with)]
